5a0c1642df8f3f8b3a063dadc2b2179091696701
Daniel <ardekanicapital@gmail.com>
2020-08-06 11:22:46 -0400

    rename positiveInt to numberRows

--- problem_5/da2753_hw4_q5.cpp
+++ problem_5/da2753_hw4_q5.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-    int positiveInt, spacing, print, i;
+    int numberRows, spacing, print, i;
 
     cout << "Please enter a positive integer: ";
-    cin >> positiveInt;
+    cin >> numberRows;
 
     //This loop runs through the first
     //half of the hourglass
-    for(i = 1; i <= positiveInt; i++) {
+    for(i = 1; i <= numberRows; i++) {
 
         //This for loop provides
         //indentation for each new row
@@ -19,10 +19,13 @@ int main()
 
         //This for loop prints out the *
         //for each new row incrementing by 1 each iteration
-        for(print = i; print <= positiveInt; print++)
+        for(print = i; print <= numberRows; print++)
             cout << "*" << " ";
 
         //This cout statement ends each row.
         cout << endl;
     }
+
+    //This loop will work in reverse from the first half
+    //decrement numberRows by one every iteration
 }
