b7c2d9ca3c890a3fa058d59f0177ecfed76e783d
Daniel <ardekanicapital@gmail.com>
2020-08-08 12:50:25 -0400

    Version one complete. Version two in progress.

--- problem_4/da2753_hw4_q4.cpp
+++ problem_4/da2753_hw4_q4.cpp
@@ -5,18 +5,50 @@ using namespace std;
 
 int main()
 {
-    int sequence, userInput, sum;
-    float ;
+//    //SECTION A --------------------------------- SECTION A//
 
-    cout << "section a" << endl;
-    cout << "Please enter the length of the sequence: ";
-    cin >> sequence;
-    cout << "Please enter your sequence: " << endl;
+//    double sequenceLength, geoMean, exponent, num, base = 1.0;
+//
+//    cout << "section a" << endl;
+//    cout << "Please enter the length of the sequence: ";
+//    cin >> sequenceLength;
+//    cout << "Please enter your sequence: " << endl;
+//
+//    for (int i = 1; i <= sequenceLength; i++)
+//    {
+//        cin >> num;
+//        base = base * num;
+//    }
+//    exponent = sequenceLength;
+//
+//    geoMean = pow(base, 1/exponent);
+//    cout << "The geometric mean is: " << geoMean << endl;
 
-    for (int i = 1; i <= sequence; i++)
+    //SECTION B --------------------------------- SECTION B//
+
+    double geoMeanB, exponentB, numB, exit, baseB = 1.0;
+
+    cout << "section B" << endl;
+    cout << "Please enter a non-empty sequence of positive integers, each one in a separate line. End your sequence by typing -1: " << endl;
+
+    int i = 1;
+    while (exit != -1)
     {
-        cin >> userInput;
+        cin >> numB;
+        cin >> exit;
+        baseB = baseB * numB;
+        i++;
+
+        if (numB == -1)
+        {
+            numB = 0;
+            baseB = baseB * numB;
+        }
     }
-    cout << userInput << endl;
+    exponentB = i;
+    geoMeanB = pow(baseB, 1/exponentB);
+    cout << "exponent is: " << i << endl;
+    cout << "The geometric mean is: " << geoMeanB << endl;
+
     return 0;
 }
