2c4e8749012ad66945b7bd6526e701481cf44a56
Daniel <ardekanicapital@gmail.com>
2020-08-07 23:53:11 -0400

    clean push

---
+++ problem_4/da2753_hw4_q4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int sequence, userInput, sum;
+    float ;
+
+    cout << "section a" << endl;
+    cout << "Please enter the length of the sequence: ";
+    cin >> sequence;
+    cout << "Please enter your sequence: " << endl;
+
+    for (int i = 1; i <= sequence; i++)
+    {
+        cin >> userInput;
+    }
+    cout << userInput << endl;
+    return 0;
+}
